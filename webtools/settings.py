00e10859284a1e582d95d0616572fd058bcce998
Anisimoff <happyprooo@gmail.com>
2024-02-07 17:00:02 +0300

    settings fix

--- webtools/settings.py
+++ webtools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-il7z=@=amv#_)$j2ovjb_21ht1uxw*9w&0^il!wbcjxqes+-sv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
