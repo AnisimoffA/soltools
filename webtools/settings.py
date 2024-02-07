6ba8d0f760b163426b3088f689e03275c6cf7583
Anisimoff <happyprooo@gmail.com>
2024-02-07 17:21:24 +0300

    db fix

--- webtools/settings.py
+++ webtools/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,14 +80,11 @@ WSGI_APPLICATION = 'webtools.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "soltools",
-        "USER": "anisimoff",
-        "PASSWORD": 1332,
-        "HOST": "localhost",
-        "PORT": 5432,
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 #
 # DATABASES = {
